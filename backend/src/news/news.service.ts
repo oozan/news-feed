dccfd9ab276389d57f04fa20c267f3829d3f939c
oozan <oozayranci@gmail.com>
2023-10-05 15:16:53 +0300

    News controller and module created to retrieve data from the db

---
+++ backend/src/news/news.service.ts
@@ -0,0 +1,19 @@
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { News } from './news.entity';
+import { Injectable } from '@nestjs/common';
+
+// NewsService consists of the usual service method implementations mills.
+// The services handle the requested data which can be reused accross the application.
+@Injectable()
+export class NewsService {
+  constructor(
+    @InjectRepository(News)
+    private newsRepository: Repository<News>,
+  ) {}
+
+  // finAll finds all the news in the database.
+  async findAll() {
+    return await this.newsRepository.find();
+  }
+}
