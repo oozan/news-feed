db1b55f7f6a7db3d6659318d64b72d25407c0aa3
oozan <oozayranci@gmail.com>
2023-10-11 20:23:40 +0300

    Configures tailwindcss

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,25 @@
-import type { Config } from 'tailwindcss'
+const colors = require("tailwindcss/colors");
 
-const config: Config = {
+const colorScopes = require("./constants/colorScope.json");
+const allColors = { ...colorScopes };
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    colors: {
+      transparent: "transparent",
+      current: "currentColor",
+      black: colors.black,
+      white: colors.white,
+      ...allColors,
     },
+    fontFamily: {}, // Removing default Tailwind CSS fonts
+    extend: {},
   },
-  plugins: [],
-}
-export default config
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
