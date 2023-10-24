4d4d9caf0f8207566b4901929bb53bd42abe5c8c
oozan <oozayranci@gmail.com>
2023-10-24 23:51:31 +0300

    Set up i18n configs

---
+++ frontend/i18n/i18n.ts
@@ -0,0 +1,42 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+
+import lang_en from "./locales/en.json";
+
+export type AvailableLanguages = keyof typeof languageResources;
+
+const defaultNS = "translation";
+export const languageResources = {
+  en: {
+    translation: lang_en,
+  },
+};
+
+// i18n configuration
+i18n.use(initReactI18next).init({
+  compatibilityJSON: "v3",
+  lng: "en",
+  ns: [defaultNS],
+  resources: languageResources,
+  defaultNS,
+  debug: false,
+  // cache: {
+  //   enabled: true
+  // },
+  interpolation: {
+    escapeValue: false,
+  },
+});
+
+type I18nType = {
+  t: (key: string, options?: { [key: string]: string | number }) => string;
+};
+
+export const I18n = i18n as I18nType;
+
+declare module "i18next" {
+  interface CustomTypeOptions {
+    defaultNS: typeof defaultNS;
+    resources: (typeof languageResources)["en"];
+  }
+}
