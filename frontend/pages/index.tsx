fac8669215472247d308c844b8b0747b499ca6a8
oozan <oozayranci@gmail.com>
2023-11-26 20:58:08 +0200

    Index Page with new texts

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
 import { Layout } from "@layouts/layout/layout";
-
-const inter = Inter({ subsets: ["latin"] });
+import { useTranslation } from "react-i18next";
 
 export default function Home() {
-  return <Layout>TEST</Layout>;
+  const { t } = useTranslation();
+  return (
+    <Layout>
+      <div className="flex flex-col items-center justify-center">
+        <h1 className="text-3xl font-bold"> {t("common.initial")}</h1>
+        <p className="text-center mt-6">{t("common.paragraph_1")}</p>
+        <p className="text-center">{t("common.paragraph_2")}</p>
+      </div>
+    </Layout>
+  );
 }
