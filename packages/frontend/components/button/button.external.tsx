5068a4ac98986e27e069e35f93983474e9ded310
oozan <oozayranci@gmail.com>
2023-12-05 19:26:06 +0200

    External Button

---
+++ packages/frontend/components/button/button.external.tsx
@@ -0,0 +1,31 @@
+import clsx from "clsx";
+
+import { ButtonBaseProps, buttonIconClasses } from "./button";
+
+import { Icon } from "@elements/icon/icon";
+
+interface ButtonExternalProps {
+  children: ButtonBaseProps["children"];
+  className?: string;
+  url: string;
+  testId?: string;
+  icon?: ButtonBaseProps["icon"];
+}
+
+export const ButtonExternal = ({
+  children,
+  className = "",
+  url,
+  testId,
+  icon,
+}: ButtonExternalProps): JSX.Element => (
+  <a
+    href={url}
+    className={clsx({ [className]: true })}
+    data-testid={testId ?? "button-external"}
+    rel="noreferrer nofollow"
+  >
+    {children}
+    {icon && <Icon icon={icon} className={buttonIconClasses} />}
+  </a>
+);
