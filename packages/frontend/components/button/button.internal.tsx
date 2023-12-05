5b9c6fb46ca07ac2e25a3cc6e5d244b6bd0530bd
oozan <oozayranci@gmail.com>
2023-12-05 19:27:51 +0200

    Internal Button

---
+++ packages/frontend/components/button/button.internal.tsx
@@ -0,0 +1,31 @@
+import clsx from "clsx";
+import Link from "next/link";
+
+import { ButtonBaseProps, buttonIconClasses } from "./button";
+
+import { Icon } from "@elements/icon/icon";
+
+interface ButtonInternalProps {
+  children: ButtonBaseProps["children"];
+  className?: string;
+  url: string;
+  testId?: string;
+  icon?: ButtonBaseProps["icon"];
+}
+
+export const ButtonInternal = ({
+  children,
+  className = "",
+  url,
+  testId,
+  icon,
+}: ButtonInternalProps): JSX.Element => (
+  <Link
+    href={url}
+    className={clsx({ [className]: true })}
+    data-testid={testId ?? "button-internal"}
+  >
+    {children}
+    {icon && <Icon icon={icon} className={buttonIconClasses} />}
+  </Link>
+);
