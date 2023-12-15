bba725cec402055e3b26459cb1e2beb89ac241ac
oozan <oozayranci@gmail.com>
2023-12-15 16:58:22 +0200

    Testing the data from the backend

--- packages/frontend/pages/news.tsx
+++ packages/frontend/pages/news.tsx
@@ -1,5 +1,42 @@
 import { Layout } from "@layouts/layout/layout";
 
-export default function News() {
-  return <Layout>TEST</Layout>;
+interface NewsProps {
+  data: any; // Replace 'any' with the actual type of your data if possible
 }
+
+const NewsPage: React.FC<NewsProps> = ({ data }) => {
+  return (
+    <Layout>
+      <h1>Testing The Data from the backend</h1>
+      {data ? (
+        <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>Loading data...</p>
+      )}
+    </Layout>
+  );
+};
+
+export async function getServerSideProps() {
+  const backendUrl = "http://localhost:3001/news-feed/v1/news"; // Adjust the API endpoint as needed
+
+  try {
+    const response = await fetch(backendUrl);
+    const data = await response.json();
+
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return {
+      props: {
+        data: null,
+      },
+    };
+  }
+}
+
+export default NewsPage;
