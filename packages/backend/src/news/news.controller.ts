d565948fcc8da35cb5c8dcd871e86d3d8497019b
oozan <oozayranci@gmail.com>
2023-12-18 18:12:32 +0200

    New error handling added

--- packages/backend/src/news/news.controller.ts
+++ packages/backend/src/news/news.controller.ts
@@ -2,6 +2,8 @@ import {
   ClassSerializerInterceptor,
   Controller,
   Get,
+  HttpException,
+  HttpStatus,
   UseInterceptors,
 } from '@nestjs/common';
 import { NewsService } from './news.service';
@@ -12,7 +14,7 @@ import { NewsDTO } from './dto/news.dto';
 // Newa controller receives /news requests for the client.
 @UseInterceptors(ClassSerializerInterceptor)
 @ApiTags('Version 1')
-@Controller({ version: '1' })
+@Controller('/news-feed/v1/news')
 export class NewsController {
   constructor(private readonly newsService: NewsService) {}
 
@@ -22,9 +24,18 @@ export class NewsController {
   @ApiResponse({
     status: 200,
     description: 'List of news that provide data to the database of this API.',
+    type: NewsDTO,
+    isArray: true,
   })
   async getAll() {
     const newsEntities = await this.newsService.findAll();
-    return convertEntitiesToDTOs(newsEntities, NewsDTO);
+    try {
+      return convertEntitiesToDTOs(newsEntities, NewsDTO);
+    } catch (e: any) {
+      throw new HttpException(
+        { error: 'News Not found or user has no access to it' },
+        HttpStatus.BAD_REQUEST,
+      );
+    }
   }
 }
