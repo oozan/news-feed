83e9aa4e7c44b93e9cd9f9018389118e9eff5a90
oozan <oozayranci@gmail.com>
2023-12-27 15:36:20 +0200

    Basic redux fetch for news

--- packages/frontend/pages/news/index.tsx
+++ packages/frontend/pages/news/index.tsx
@@ -1,10 +1,41 @@
-import { useTranslation } from 'react-i18next';
+// news/index.tsx
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchNews, selectNews } from '@redux/NewsSlice';
+import { AppDispatch } from '@redux/store';
 
-import { Heading } from '@elements/heading/heading';
-import { Layout } from '@layouts/layout/layout';
+const NewsPage = () => {
+  const dispatch: AppDispatch = useDispatch(); // Explicitly type dispatch with AppDispatch
+  const { news, status, error } = useSelector(selectNews);
 
-export default function News() {
-  const { t } = useTranslation();
+  useEffect(() => {
+    // Dispatch the fetchNews action when the component mounts
+    dispatch(fetchNews());
+  }, [dispatch]);
 
-  return <Layout>TEST</Layout>;
-}
+  // Handling different status states
+  if (status === 'loading') {
+    return <p>Loading...</p>;
+  }
+
+  if (status === 'failed') {
+    return <p>Error: {error}</p>;
+  }
+
+  // Render the news data
+  return (
+    <div>
+      <h1>News Page</h1>
+      <ul>
+        {news.map((item) => (
+          <li key={item.id}>
+            <h3>{item.title}</h3>
+            <p>{item.content}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default NewsPage;
