066249c4c16d6290363e80a8068ea6d20e6fc809
oozan <oozayranci@gmail.com>
2024-01-04 10:42:48 +0200

    generated fixes

--- packages/frontend/api-config/openapi-config-news.js
+++ packages/frontend/api-config/openapi-config-news.js
@@ -1,8 +1,8 @@
 /** @type {import('@rtk-query/codegen-openapi').ConfigFile} */
 module.exports = {
   schemaFile: '../redux/schema/api-spec-news.json',
-  apiFile: '../redux/api/emptyPortalApi.ts',
-  apiImport: 'emptyPortalApi',
+  apiFile: '../redux/api/emptyNewsApi.ts',
+  apiImport: 'emptyNewsApi',
   outputFile: '../redux/api/generated/NewsApi.ts',
   exportName: 'NewsApi',
   hooks: { lazyQueries: true, mutations: true, queries: true },
