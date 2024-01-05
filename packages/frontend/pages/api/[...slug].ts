5225ecc404556222bfe36332455063a54008a733
oozan <oozayranci@gmail.com>
2024-01-05 11:33:37 +0200

    Refactor news fetching logic to use Redux Toolkit's createSlice and createAsyncThunk

--- packages/frontend/pages/api/[...slug].ts
+++ packages/frontend/pages/api/[...slug].ts
@@ -1,21 +1,25 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function (app: any) {
-  app.use(
-    '/api/news-feed/news',
-    createProxyMiddleware({
-      target: 'http://localhost:3001', // Your backend server URL
-      changeOrigin: true,
-      pathRewrite: {
-        '^/api/news-feed/news': '/news-feed/news',
-      },
-      onProxyRes: (proxyRes: any, req: any) => {
-        const exchange = `[HPM] ${req.method} ${req.url} -> ${proxyRes.req.protocol}//${proxyRes.req.host}${proxyRes.req.path} [${proxyRes.statusCode}]`;
-        console.log(exchange);
-      },
-    })
-  );
+const { BACKEND_URL_API, NEXT_PUBLIC_IS_LOCAL_BACKEND } = process.env;
+const isLocalBackend = NEXT_PUBLIC_IS_LOCAL_BACKEND?.toLocaleLowerCase() === 'true';
+
+const customRouter = () => {
+  return isLocalBackend ? 'http://localhost:3001' : BACKEND_URL_API;
 };
+
+const proxy = createProxyMiddleware({
+  router: customRouter,
+  secure: true,
+  changeOrigin: true,
+  pathRewrite: { '^/api': '/news-feed/news' }, // Update this line
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onProxyRes: (proxyRes: any, req: any) => {
+    const exchange = `[HPM] ${req.method} ${req.url} -> ${proxyRes.req.protocol}//${proxyRes.req.host}${proxyRes.req.path} [${proxyRes.statusCode}]`;
+    console.log(exchange);
+  },
+});
+
+export default proxy;
 
 export const config = {
   api: {
